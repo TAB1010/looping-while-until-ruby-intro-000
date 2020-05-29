acaa90f9070f8cbe91b72c9216fa558d4fd3f838
KHANDKER HASSAN <khassanasgar@gmail.com>
2020-05-29 02:30:16 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,6 @@
-def using_while
-  levitation_force = 6
-	
-  #your code here
-end
-
-
+def using_until
+         levitation_force = 6		         levitation_force = 6
+           puts "Wingardium Leviosa"
+           levitation_force += 1
+         end
+ end		
